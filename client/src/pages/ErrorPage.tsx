9c91f0ab5ac9ae378ad0c6370ec839ebf026d49b
Nokeen <cordrepayne@gmail.com>
2022-11-15 23:39:25 -0600

    Created and export pages

---
+++ client/src/pages/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+function ErrorPage(){
+    return (
+        <div style={{ color: "#ae00ff", fontSize: "5rem", textAlign: "center" }}>
+            Oops 404 Not Found!
+        </div>
+    )
+}
+export default ErrorPage;
